dadc138ff6e0cc569ef0a4b10bfcf38da0e49867
Julia <julia.rogovaja@gmail.com>
2023-07-15 10:18:02 +0300

    .

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,18 +4,18 @@
 // выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-Console.Write("Введите количество элементов массива: ");
+Console.Write("Enter the number of array elements: ");
 int m = Convert.ToInt32(Console.ReadLine());
 string[] stringArray = new string[m];
 void array(string[] stringArray)
 {
     for (int i = 0; i < stringArray.Length; i++)
     {
-        Console.WriteLine($"Введите {i + 1} элемент массива");
+        Console.WriteLine($"Enter {i + 1} element of array");
         stringArray[i] = Console.ReadLine();
     }
 }
-string[] symbol(string[] stringArray)
+string[] numbers(string[] stringArray)
 {
     int n = 0;
     for (int i = 0; i < stringArray.Length; i++)
@@ -35,7 +35,7 @@ string[] symbol(string[] stringArray)
     }
     return rez;
 }
-void printA(string[] stringArray)
+void print(string[] stringArray)
 {
     Console.Write("[");
     for (int i = 0; i < stringArray.Length; i++)
@@ -45,4 +45,4 @@ void printA(string[] stringArray)
     Console.Write("]");
 }
 array(stringArray);
-printA(symbol(stringArray));
+print(numbers(stringArray));
